63eb7b86fddf2056d971a79a0051c6e31ef5f5fa
Mikhail Gordeev <onitself@gmail.com>
2021-05-05 21:42:30 +0300

    Update mobile header

--- scripts/header.js
+++ scripts/header.js
@@ -3,8 +3,10 @@
     "<div>" +
         "<img class='logo' src='logo.png'></img>" +  
         "<div onclick='headerButtonsClick()' class='menu_button' id='menu_button'>" +
+            "<img class='button_image' src='menu.png'></img>" +
         "</div>" +
         "<div onclick='headerButtonsClick()' class='close_button' id='close_button'>" +
+            "<img class='button_image' src='close.png'></img>" +
         "</div>" +
     "</div>" +   
 "</div>" 
