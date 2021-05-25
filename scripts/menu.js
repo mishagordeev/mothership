6ad9d516593134f39883e53ff90c96136e250bf5
Mikhail Gordeev <onitself@gmail.com>
2021-05-25 19:04:42 +0300

    Edit menu

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,18 +1,18 @@
 let menu = "<div id='menu' class='menu'>" +
-"<p>› <a href='./index.html'><b>Создание персонажа</b></a></p>" +
-"<p>› <a href='./basic-rules.html'><b>Основные правила</b></a></p>" +
-"<p>› <a href='./skills.html'><b>Навыки</b></a></p>" +
-"<p>› <a href='./saves.html'><b>Испытания</b></a></p>" +
-"<p>› <a href='./survival.html'><b>Выживание</b></a></p>" +
-"<p>› <a href='./combat.html'><b>Бой</b></a></p>" +
-"<p>› <a href='./weapons.html'><b>Оружие</b></a></p>" +
-"<p>› <a href='./armor.html'><b>Броня</b></a></p>" +
-"<p>› <a href='./equipment.html'><b>Предметы</b></a></p>" +
-"<!-- <p>› <a href=''><b>Безделушки</b></a></p> -->" +
+"<p><a href='./index.html'><b>Создание персонажа »</b></a></p>" +
+"<p><a href='./basic-rules.html'><b>Основные правила »</b></a></p>" +
+"<p><a href='./skills.html'><b>Навыки »</b></a></p>" +
+"<p><a href='./saves.html'><b>Испытания »</b></a></p>" +
+"<p><a href='./survival.html'><b>Выживание »</b></a></p>" +
+"<p><a href='./combat.html'><b>Бой »</b></a></p>" +
+"<p><a href='./weapons.html'><b>Оружие »</b></a></p>" +
+"<p><a href='./armor.html'><b>Броня »</b></a></p>" +
+"<p><a href='./equipment.html'><b>Предметы »</b></a></p>" +
+"<!-- <p><a href=''><b>Безделушки</b></a></p> -->" +
 "<!-- <p>› <a href=''><b>Нашивки</b></a></p> -->" +
-"<p>› <a href='./stress.html'><b>Стресс</b></a></p>" +
-"<p>› <a href='./panic.html'><b>Паника и решимость</b></a></p>" +
-"<p>› <a href='./experience.html'><b>Опыт и уровни</b></a></p>" +
+"<p><a href='./stress.html'><b>Стресс »</b></a></p>" +
+"<p><a href='./panic.html'><b>Паника и решимость »</b></a></p>" +
+"<p><a href='./experience.html'><b>Опыт и уровни »</b></a></p>" +
 "<br>" +
 "<p><a href='./files/Mothership_Character_Sheet_Rus_v1.3.pdf' target='_blank'><b>Лист персонажа</b></a></p>" +
 "</div>"
