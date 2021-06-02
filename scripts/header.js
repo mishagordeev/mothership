ff87c66aca25cdff98861866b904f53717bda647
Mikhail Gordeev <onitself@gmail.com>
2021-06-02 17:55:17 +0300

    Edit mobile view menu

--- scripts/header.js
+++ scripts/header.js
@@ -1,13 +1,8 @@
- let header = 
- "<div id='header' class='header'>" +
-    "<div>" +
-        "<img class='logo' src='logo.png'></img>" +  
-        "<div onclick='headerButtonsClick()' class='menu_button' id='menu_button'>" +
-            "<img class='button_image' src='menu.png'></img>" +
-        "</div>" +
-        "<div onclick='headerButtonsClick()' class='close_button' id='close_button'>" +
-            "<img class='button_image' src='close.png'></img>" +
-        "</div>" +
-    "</div>" +   
-"</div>" 
+let header = 
+"<div id='header' class='header'>" +
+"<div class='logo_container'><img src='logo.png' class='logo'></img></div>" +
+"<div style='flex-grow: 1;'></div>" +
+"<div onclick='headerButtonsClick()' class='menu_button' id='menu_button'><img class='button_image' src='menu_button.png'></img></div>" +
+"<div onclick='headerButtonsClick()' class='close_button' id='close_button'><img class='button_image' src='close_button.png'></img></div>" +
+"</div>"
 document.body.innerHTML = header;
