8620d69eb8139f996666ef23b339dbe2338d246d
Mikhail Gordeev <onitself@gmail.com>
2021-06-02 20:18:48 +0300

    Add links style

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,20 +1,20 @@
 let menu = "<div id='menu' class='menu'>" +
-"<p><a href='./'><b>Создание персонажа »</b></a></p>" +
-"<p><a href='./basic-rules.html'><b>Основные правила »</b></a></p>" +
-"<p><a href='./skills.html'><b>Навыки »</b></a></p>" +
-"<p><a href='./saves.html'><b>Испытания »</b></a></p>" +
-"<p><a href='./survival.html'><b>Выживание »</b></a></p>" +
-"<p><a href='./combat.html'><b>Бой »</b></a></p>" +
-"<p><a href='./weapons.html'><b>Оружие »</b></a></p>" +
-"<p><a href='./armor.html'><b>Броня »</b></a></p>" +
-"<p><a href='./equipment.html'><b>Предметы »</b></a></p>" +
-"<p><a href='./trinkets.html'><b>Безделушки »</b></a></p>" +
-"<p><a href='./patches.html'><b>Нашивки »</b></a></p>" +
-"<p><a href='./stress.html'><b>Стресс »</b></a></p>" +
-"<p><a href='./panic.html'><b>Паника и решимость »</b></a></p>" +
-"<p><a href='./experience.html'><b>Опыт и уровни »</b></a></p>" +
+"<p><a class='internal' href='./'><b>Создание персонажа</b></a></p>" +
+"<p><a class='internal' href='./basic-rules.html'><b>Основные правила</b></a></p>" +
+"<p><a class='internal' href='./skills.html'><b>Навыки</b></a></p>" +
+"<p><a class='internal' href='./saves.html'><b>Испытания</b></a></p>" +
+"<p><a class='internal' href='./survival.html'><b>Выживание</b></a></p>" +
+"<p><a class='internal' href='./combat.html'><b>Бой</b></a></p>" +
+"<p><a class='internal' href='./weapons.html'><b>Оружие</b></a></p>" +
+"<p><a class='internal' href='./armor.html'><b>Броня</b></a></p>" +
+"<p><a class='internal' href='./equipment.html'><b>Предметы</b></a></p>" +
+"<p><a class='internal' href='./trinkets.html'><b>Безделушки</b></a></p>" +
+"<p><a class='internal' href='./patches.html'><b>Нашивки</b></a></p>" +
+"<p><a class='internal' href='./stress.html'><b>Стресс</b></a></p>" +
+"<p><a class='internal' href='./panic.html'><b>Паника и решимость</b></a></p>" +
+"<p><a class='internal' href='./experience.html'><b>Опыт и уровни</b></a></p>" +
 "<br>" +
-"<p><a href='./files/Mothership_Character_Sheet_Rus_v1.3.pdf' target='_blank'><b>Лист персонажа ↓</b></a></p>" +
+"<p><a class='download' href='./files/Mothership_Character_Sheet_Rus_v1.3.pdf' target='_blank'><b>Лист персонажа</b></a></p>" +
 "</div>"
 let wrapper = document.getElementById("wrapper");
 wrapper.innerHTML = menu;
