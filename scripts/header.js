80a822d244243dd0448780a9ef2a7d2fa2953845
Mikhail Gordeev <onitself@gmail.com>
2021-06-03 11:04:55 +0300

    Refactor files

--- scripts/header.js
+++ scripts/header.js
@@ -1,8 +1,8 @@
 let header = 
 "<div id='header' class='header'>" +
-"<div class='logo_container'><img src='logo.png' class='logo'></img></div>" +
+"<div class='logo_container'><img src='./images/logo.png' class='logo'></img></div>" +
 "<div style='flex-grow: 1;'></div>" +
-"<div onclick='headerButtonsClick()' class='menu_button' id='menu_button'><img class='button_image' src='menu_button.png'></img></div>" +
-"<div onclick='headerButtonsClick()' class='close_button' id='close_button'><img class='button_image' src='close_button.png'></img></div>" +
+"<div onclick='headerButtonsClick()' class='menu_button' id='menu_button'><img class='button_image' src='./images/menu_button.png'></img></div>" +
+"<div onclick='headerButtonsClick()' class='close_button' id='close_button'><img class='button_image' src='./images/close_button.png'></img></div>" +
 "</div>"
 document.body.innerHTML = header;
