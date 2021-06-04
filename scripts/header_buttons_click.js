149720efd6a61add0e4d2e627eee4ae9c6a85665
Mikhail Gordeev <onitself@gmail.com>
2021-06-04 10:47:43 +0300

    Add menu color

--- scripts/header_buttons_click.js
+++ scripts/header_buttons_click.js
@@ -3,16 +3,19 @@
           var content_container = document.getElementById("content_container");
           var menuButton = document.getElementById("menu_button");
           var closeButton = document.getElementById("close_button");
+          var wrapper = document.getElementById("wrapper");
           if (menu.style.display === "block") {
             menu.style.display = "none";
             content_container.style.display = "block";
             menuButton.style.display = "block";
             closeButton.style.display = "none";
+            wrapper.style.backgroundColor = "#fff"
           } else {
             menu.style.display = "block";
             menu.scrollTop = 0;
             content_container.style.display = "none";
             menuButton.style.display = "none";
             closeButton.style.display = "block";
+            wrapper.style.backgroundColor = "#ddd"
           }
         }
