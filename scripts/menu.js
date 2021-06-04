b3187c6eb77586a1f1c5b581ac6dbf29ff995b1d
Mikhail Gordeev <onitself@gmail.com>
2021-06-04 10:47:57 +0300

    Edit menu style

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,20 +1,26 @@
 let menu = "<div id='menu' class='menu'>" +
-"<p><a class='internal' href='./'><b>Создание персонажа</b></a></p>" +
-"<p><a class='internal' href='./basic-rules.html'><b>Основные правила</b></a></p>" +
-"<p><a class='internal' href='./skills.html'><b>Навыки</b></a></p>" +
-"<p><a class='internal' href='./saves.html'><b>Испытания</b></a></p>" +
-"<p><a class='internal' href='./survival.html'><b>Выживание</b></a></p>" +
-"<p><a class='internal' href='./combat.html'><b>Бой</b></a></p>" +
-"<p><a class='internal' href='./weapons.html'><b>Оружие</b></a></p>" +
-"<p><a class='internal' href='./armor.html'><b>Броня</b></a></p>" +
-"<p><a class='internal' href='./equipment.html'><b>Предметы</b></a></p>" +
-"<p><a class='internal' href='./trinkets.html'><b>Безделушки</b></a></p>" +
-"<p><a class='internal' href='./patches.html'><b>Нашивки</b></a></p>" +
-"<p><a class='internal' href='./stress.html'><b>Стресс</b></a></p>" +
-"<p><a class='internal' href='./panic.html'><b>Паника и решимость</b></a></p>" +
-"<p><a class='internal' href='./experience.html'><b>Опыт и уровни</b></a></p>" +
+"<h2>Руководство по выживанию</h2>" +
+"<div class='list'>" + 
+"<p><a class='internal' href='./'>Создание персонажа</a></p>" +
+"<p><a class='internal' href='./basic-rules.html'>Основные правила</a></p>" +
+"<p><a class='internal' href='./skills.html'>Навыки</a></p>" +
+"<p><a class='internal' href='./saves.html'>Испытания</a></p>" +
+"<p><a class='internal' href='./survival.html'>Выживание</a></p>" +
+"<p><a class='internal' href='./combat.html'>Бой</a></p>" +
+"<p><a class='internal' href='./weapons.html'>Оружие</a></p>" +
+"<p><a class='internal' href='./armor.html'>Броня</a></p>" +
+"<p><a class='internal' href='./equipment.html'>Предметы</a></p>" +
+"<p><a class='internal' href='./trinkets.html'>Безделушки</a></p>" +
+"<p><a class='internal' href='./patches.html'>Нашивки</a></p>" +
+"<p><a class='internal' href='./stress.html'>Стресс</a></p>" +
+"<p><a class='internal' href='./panic.html'>Паника и решимость</a></p>" +
+"<p><a class='internal' href='./experience.html'>Опыт и уровни</a></p>" +
+"</div>" +
 "<br>" +
-"<p><a class='download' href='./files/Mothership_Character_Sheet_Rus_v1.3.pdf' target='_blank'><b>Лист персонажа</b></a></p>" +
+"<h2>Дополнительные материалы</h2>" +
+"<div class='list'>" + 
+"<p><a class='download' href='./files/Mothership_Character_Sheet_Rus_v1.3.pdf' target='_blank'>Лист персонажа</a></p>" +
+"</div>" +
 "</div>"
 let wrapper = document.getElementById("wrapper");
 wrapper.innerHTML = menu;
