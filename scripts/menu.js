3d0459d6a71ac27fd37dcb83f8a1c907d4b44414
Mikhail Gordeev <onitself@gmail.com>
2021-11-12 23:21:06 +0300

    Edit character sheet

--- scripts/menu.js
+++ scripts/menu.js
@@ -20,7 +20,7 @@ let menu = "<div id='menu' class='menu'>" +
 "</div>" +
 "<h3>Дополнительные материалы</h3>" +
 "<div class='list'>" + 
-"<p><a class='download' href='./files/Mothership_Character_Sheet_Rus_v1.4.pdf' target='_blank'>Лист персонажа</a></p>" +
+"<p><a class='download' href='./files/Mothership_Character_Sheet_Ru_v1.5.pdf' target='_blank'>Лист персонажа</a></p>" +
 "<p><a class='download' href='./files/Mothership_Mercenaries_Sheet_Ru_v1.0.pdf' target='_blank'>Листы наёмников</a></p>" +
 "</div>" +
 "</div>"
