39c857d07f87573b437e6bdfa227bc951b0b7bd2
Mikhail Gordeev <onitself@gmail.com>
2022-01-01 00:59:49 +0300

    Remove unnecessary file

--- header.js
+++
@@ -1,8 +0,0 @@
-let header = 
-"<div id='header' class='header'>" +
-"<div class='logo_container'><p class='logo'>MOTHERSHIP</p></div>" +
-"<div style='flex-grow: 1;'></div>" +
-"<div onclick='headerButtonsClick()' class='menu_button' id='menu_button'><img class='button_image' src='./images/menu_button.png'></img></div>" +
-"<div onclick='headerButtonsClick()' class='close_button' id='close_button'><img class='button_image' src='./images/close_button.png'></img></div>" +
-"</div>"
-document.body.innerHTML = header;
