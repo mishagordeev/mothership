b24e3683f1e795e2f39beef1873f14546337c026
Mikhail Gordeev <onitself@gmail.com>
2022-01-06 10:48:09 +0300

    Edit footer years

--- scripts/footer.js
+++ scripts/footer.js
@@ -1,7 +1,7 @@
 let footer =
 "<div class='footer'>" +
     "<p>" +
-        "© 2021 <a style='color: white;' class='external' href='https://t.me/mishagordeev' target='_blank'>Михаил Гордеев</a>" +
+        "© 2021-2022 <a style='color: white;' class='external' href='https://t.me/mishagordeev' target='_blank'>Михаил Гордеев</a>" +
     "</p>" +
     "<p>" +
         "Не является официальным переводом <a style='color: white;' class='external' href='https://www.mothershiprpg.com/' target='_blank'>Mothership</a>. Оригинальная игра принадлежит <a style='color: white;' class='external' href='https://www.tuesdayknightgames.com/' target='_blank'>Tuesday Knight Games</a>." +
