66127a0af5793312ce2974dc30c4f728377c182f
Mikhail Gordeev <onitself@gmail.com>
2022-01-30 12:17:00 +0300

    Edit links

--- scripts/footer.js
+++ scripts/footer.js
@@ -1,10 +1,10 @@
 let footer =
 "<div class='footer'>" +
     "<p>" +
-        "© 2021-2022 <a style='color: white;' class='external' href='https://t.me/mishagordeev' target='_blank'>Михаил Гордеев</a>" +
+        "© 2021-2022 <a class='external' href='https://t.me/mishagordeev' target='_blank'>Михаил Гордеев</a>" +
     "</p>" +
     "<p>" +
-        "Не является официальным переводом <a style='color: white;' class='external' href='https://www.mothershiprpg.com/' target='_blank'>Mothership</a>. Оригинальная игра принадлежит <a style='color: white;' class='external' href='https://www.tuesdayknightgames.com/' target='_blank'>Tuesday Knight Games</a>" +
+        "Не является официальным переводом <a class='external' href='https://www.mothershiprpg.com/' target='_blank'>Mothership</a>. Оригинальная игра принадлежит <a class='external' href='https://www.tuesdayknightgames.com/' target='_blank'>Tuesday Knight Games</a>" +
     "</p>" +        
 "</div>"
 document.body.innerHTML += footer;
