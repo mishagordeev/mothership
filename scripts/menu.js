121e8a7d63baaa4a1fece7710cb4a90c406ffd41
Mikhail Gordeev <onitself@gmail.com>
2022-02-06 11:58:12 +0300

    Edit internal links

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,24 +1,24 @@
 let menu = "<div id='menu' class='menu inactive'>" +
 "<h3>Руководство по выживанию</h3>" +
 "<ul>" +
-"<li><a class='internal' href='./'>Создание персонажа</a></li>" +
-"<li><a class='internal' href='./basic-rules.html'>Основные правила</a></li>" +
-"<li><a class='internal' href='./skills.html'>Навыки</a></li>" +
-"<li><a class='internal' href='./saves.html'>Испытания</a></li>" +
-"<li><a class='internal' href='./survival.html'>Выживание</a></li>" +
-"<li><a class='internal' href='./combat.html'>Бой</a></li>" +
-"<li><a class='internal' href='./weapons.html'>Оружие</a></li>" +
-"<li><a class='internal' href='./armor.html'>Броня</a></li>" +
-"<li><a class='internal' href='./equipment.html'>Предметы</a></li>" +
-"<li><a class='internal' href='./trinkets.html'>Безделушки</a></li>" +
-"<li><a class='internal' href='./patches.html'>Нашивки</a></li>" +
-"<li><a class='internal' href='./mercenaries.html'>Наёмники</a></li>" +
-"<li><a class='internal' href='./stress.html'>Стресс</a></li>" +
-"<li><a class='internal' href='./panic.html'>Паника и решимость</a></li>" +
-"<li><a class='internal' href='./space-travel.html'>Путешествия в космосе</a></li>" +
-"<li><a class='internal' href='./ship-design.html'>Проектирование корабля</a></li>" +
-"<li><a class='internal' href='./ship-combat.html'>Космические сражения</a></li>" +
-"<li><a class='internal' href='./experience.html'>Опыт и уровни</a></li>" +
+"<li><a href='./'>Создание персонажа</a></li>" +
+"<li><a href='./basic-rules.html'>Основные правила</a></li>" +
+"<li><a href='./skills.html'>Навыки</a></li>" +
+"<li><a href='./saves.html'>Испытания</a></li>" +
+"<li><a href='./survival.html'>Выживание</a></li>" +
+"<li><a href='./combat.html'>Бой</a></li>" +
+"<li><a href='./weapons.html'>Оружие</a></li>" +
+"<li><a href='./armor.html'>Броня</a></li>" +
+"<li><a href='./equipment.html'>Предметы</a></li>" +
+"<li><a href='./trinkets.html'>Безделушки</a></li>" +
+"<li><a href='./patches.html'>Нашивки</a></li>" +
+"<li><a href='./mercenaries.html'>Наёмники</a></li>" +
+"<li><a href='./stress.html'>Стресс</a></li>" +
+"<li><a href='./panic.html'>Паника и решимость</a></li>" +
+"<li><a href='./space-travel.html'>Путешествия в космосе</a></li>" +
+"<li><a href='./ship-design.html'>Проектирование корабля</a></li>" +
+"<li><a href='./ship-combat.html'>Космические сражения</a></li>" +
+"<li><a href='./experience.html'>Опыт и уровни</a></li>" +
 "</ul>" +
 "<h3>Дополнительные материалы</h3>" +
 "<ul>" +
