7379569af0436bbab5429fad1ca5815318e4ac40
Mikhail Gordeev <onitself@gmail.com>
2022-04-11 13:02:38 +0300

    Edit footer

--- scripts/footer.js
+++ scripts/footer.js
@@ -1,10 +1,36 @@
 let footer =
-"<div class='footer'>" +
-    "<p>" +
-        "© 2021-2022 <a class='external' href='https://t.me/mishagordeev' target='_blank'>Михаил Гордеев</a>" +
-    "</p>" +
-    "<p>" +
-        "Не является официальным переводом <a class='external' href='https://www.mothershiprpg.com/' target='_blank'>Mothership</a>. Оригинальная игра принадлежит <a class='external' href='https://www.tuesdayknightgames.com/' target='_blank'>Tuesday Knight Games</a>" +
-    "</p>" +        
-"</div>"
+`<footer>
+    <div>
+        <div>
+            <p>
+                © 2021-2022 Михаил Гордеев
+            </p>
+            <div style="font-size: 0.6rem">
+                <br>
+                <p>
+                    Mothershiprpg.ru не связан, не поддерживается, не спонсирован и не одобрен Tuesday Knight Games, LLC 
+                </p>
+                <p>
+                    Название Mothership и логотипы игры являются зарегистрированнными товарными знаками Tuesday Knight Games, LLC 
+                </p>
+                <br>
+                <p>
+                    Все права на опубликованные материалы принадлежат их авторам 
+                </p> 
+            </div>
+        </div>
+        <div style="flex-grow: 1;"></div>
+        <div class="social">
+            <p>
+                Следи за обновлениями 
+            </p> 
+            <p style="padding-top: 10px;">
+                <a href='https://t.me/mothershiprpgru' target='_blank'><img src="../images/tg.png"></a>
+                <a href='https://vk.com/mothershiprpgru' target='_blank'><img src="../images/vk.png"></a>
+                <a href='https://discord.gg/CNmnVHRazJ' target='_blank'><img src="../images/discord.png"></a>
+            </p> 
+            <br>      
+        </div>  
+    </div>
+</footer>`
 document.body.innerHTML += footer;
