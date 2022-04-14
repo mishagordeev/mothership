93cfbdc334d9c9609d064328b93c540dcbb24e60
Mikhail Gordeev <onitself@gmail.com>
2022-04-14 14:15:32 +0300

    Edit mobile menu

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,10 +1,10 @@
 let menu = `
-<div id='menu' class='menu inactive'>
-    <p>ПРАВИЛА</p>
-    <p>МОДУЛИ</p>
-    <p>ЛИСТЫ</p>
-    <p>ДОПОЛНЕНИЯ</p>
-    <p>ВИДЕО</p>
-</div>`
+<nav id='menu' class='menu inactive'>
+    <p><a href="contents.html">ПРАВИЛА</a></p>
+    <p><a href="modules.html">МОДУЛИ</a></p>
+    <p><a href="lists.html">ЛИСТЫ</a></p>
+    <p><a href="additions.html">ДОПОЛНЕНИЯ</a></p>
+    <p><a href="video.html">ВИДЕО</a></p>
+</nav>`
 let wrapper = document.getElementById("wrapper");
 wrapper.innerHTML = menu;
