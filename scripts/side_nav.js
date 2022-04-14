390a74cbfb9cf3c6ad928be38bbf6ddd514f46ac
Mikhail Gordeev <onitself@gmail.com>
2022-04-14 16:18:35 +0300

    Add side nav

---
+++ scripts/side_nav.js
@@ -0,0 +1,25 @@
+let sideNav = 
+`
+<nav class="side_menu desktop">
+<ul>
+<li><a href='./creation.html'>Создание персонажа</a></li>
+<li><a href='./basic-rules.html'>Основные правила</a></li>
+<li><a href='./skills.html'>Навыки</a></li>
+<li><a href='./saves.html'>Испытания</a></li>
+<li><a href='./survival.html'>Выживание</a></li>
+<li><a href='./combat.html'>Бой</a></li>
+<li><a href='./weapons.html'>Оружие</a></li>
+<li><a href='./armor.html'>Броня</a></li>
+<li><a href='./equipment.html'>Предметы</a></li>
+<li><a href='./trinkets.html'>Безделушки</a></li>
+<li><a href='./patches.html'>Нашивки</a></li>
+<li><a href='./mercenaries.html'>Наемники</a></li>
+<li><a href='./stress.html'>Стресс, паника, решимость</a></li>
+<li><a href='./space-travel.html'>Путешествия в космосе</a></li>
+<li><a href='./ship-design.html'>Проектирование кораблей</a></li>
+<li><a href='./ship-combat.html'>Космические сражения</a></li>
+<li><a href='./experience.html'>Опыт и уровни</a></li>
+</ul>
+</nav>
+`
+document.body.innerHTML += sideNav;
