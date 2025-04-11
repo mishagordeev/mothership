24df063380ec6e27f0f3c7a352269d8ea02a51c6
mishagordeev <onitself@gmail.com>
2025-04-11 20:04:12 +0300

    Add 1st edition rules

--- scripts/side_nav.js
+++ scripts/side_nav.js
@@ -2,23 +2,32 @@ let sideNav =
 `
 <nav class="side_menu desktop">
 <ul>
-<li><a href='./creation.html'>Создание персонажа</a></li>
-<li><a href='./basic-rules.html'>Основные правила</a></li>
-<li><a href='./skills.html'>Навыки</a></li>
-<li><a href='./saves.html'>Испытания</a></li>
-<li><a href='./survival.html'>Выживание</a></li>
-<li><a href='./combat.html'>Бой</a></li>
-<li><a href='./weapons.html'>Оружие</a></li>
-<li><a href='./armor.html'>Броня</a></li>
-<li><a href='./equipment.html'>Предметы</a></li>
+<li><a href='./creation.html'>Как создать персонажа</a></li>
+<li><a href='./loadouts.html'>Начальное снаряжение</a></li>
 <li><a href='./trinkets.html'>Безделушки</a></li>
 <li><a href='./patches.html'>Нашивки</a></li>
+<li><a href='./equipment.html'>Предметы</a></li>
+<li><a href='./weapons.html'>Огнестрельное оружие</a></li>
+<li><a href='./industrial.html'>Промышленное оборудование</a></li>
+<li><a href='./armor.html'>Броня</a></li>
+<li><a href='./safety.html'>Как быть хорошим игроком</a></li>
+<li><a href='./how-to-play.html'>Как играть</a></li>
+<li><a href='./stats.html'>Характеристики и испытания</a></li>
+<li><a href='./modifying.html'>Модификация проверок</a></li>
+<li><a href='./stress.html'>Стресс</a></li>
+<li><a href='./panic.html'>Проверки паники</a></li>
+<li><a href='./skills.html'>Навыки</a></li>
+<li><a href='./skill-training.html'>Развитие навыков</a></li>
+<li><a href='./combat.html'>Боевые столкновения</a></li>
+<li><a href='./attack-and-defense.html'>Атака и защита</a></li>
+<li><a href='./wounds-and-death.html'>Раны и смерть</a></li>
+<li><a href='./range-and-distance.html'>Расстояние и дистанция</a></li>
+<li><a href='./survival.html'>Выживание</a></li>
+<li><a href='./medical-care.html'>Медицина</a></li>
+<li><a href='./example.html'>Пример игры</a></li>
+<li><a href='./ports.html'>Порты</a></li>
+<li><a href='./shore-leave.html'>Отпуск</a></li>
 <li><a href='./mercenaries.html'>Наемники</a></li>
-<li><a href='./stress.html'>Стресс, паника, решимость</a></li>
-<li><a href='./space-travel.html'>Путешествия в космосе</a></li>
-<li><a href='./ship-design.html'>Проектирование кораблей</a></li>
-<li><a href='./ship-combat.html'>Космические сражения</a></li>
-<li><a href='./experience.html'>Опыт и уровни</a></li>
 </ul>
 </nav>
 `
