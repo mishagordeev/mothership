d62c4cbe7f19629d33b1fa4993b6146a90dc335b
mishagordeev <onitself@gmail.com>
2025-06-11 23:06:28 +0300

    Add contractors generator

--- scripts/menu.js
+++ scripts/menu.js
@@ -4,7 +4,7 @@ let menu = `
     <p><a href="/modules.html">МОДУЛИ</a></p>
     <p><a href="/lists.html">ЛИСТЫ</a></p>
     <p><a href="/additions.html">ДОПОЛНЕНИЯ</a></p>
-    <p><a href="/generators.html">ГЕНЕРАТОРЫ</a></p>
+    <p><a href="/generators/character.html">ГЕНЕРАТОРЫ</a></p>
     <p><a href="/video.html">ВИДЕО</a></p>
 </nav>`
 let wrapper = document.getElementById("wrapper");
---
+++ scripts/side_nav_contractors.js
@@ -0,0 +1,10 @@
+let sideNav = 
+`
+<nav class="side_menu desktop">
+<ul>
+<li><a href='/generators/character.html'>Генератор персонажа</a></li>
+<li><a href='/generators/contractor.html'>Генератор наемника</a></li>
+</ul>
+</nav>
+`
+document.body.innerHTML += sideNav;
